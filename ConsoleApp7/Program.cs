46dc24d35c3d755350eb82ab4682325bae69c351
amirhpanahi <panahia40@gmail.com>
2023-04-20 00:42:04 +0430

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Please write the numbers with a space");
+string a = Console.ReadLine();
+
+string[] b = a.Split(' ');
+
+int[] numbers = b.Select(int.Parse).ToArray();
+
+Console.WriteLine("Please write Target Number");
+int T = int.Parse(Console.ReadLine());
+
+Console.WriteLine(calculate(numbers));
+
+string calculate(int[] num1)
+{
+    for (int i = 0; i < num1.Length; i++)
+    {
+        for (int j = 0; j < num1.Length; j++)
+        {
+            if (num1[i] + num1[j] == T)
+            {
+                return $"{numbers[i]} +  {numbers[j]} =  {T}";
+            }
+        }
+    }
+    return "";
+}
+
+Console.ReadKey();
